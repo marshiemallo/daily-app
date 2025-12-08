77da22c2c45ccbbb797ad57f30ef6c7f81c81f56
marshiemallo <marshie.repo@gmail.com>
2025-12-08 23:10:08 +0800

    UI Done, most of functionality without saving

---
+++ lib/main.dart
@@ -0,0 +1,303 @@
+import 'package:daily_app/task.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:markdown_widget/markdown_widget.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+void main() {
+  runApp(const MyApp());
+
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String? _currentDate;
+  int _counter = 0;
+  int _journalMode = 0;
+
+  // ###################
+  // ##### Journal #####
+  // ###################
+  String _markdownContent = '';
+  final TextEditingController _markdownController = TextEditingController();
+
+  void _toggleJournal() {
+    if(_journalMode == 0){
+      setState((){_journalMode = 1;});
+    } else {
+      setState((){_journalMode = 0;});
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _markdownController.addListener(_updateContent);
+  }
+
+  void _updateContent() {
+    setState((){
+      _markdownContent = _markdownController.text;
+    });
+  }
+
+  Widget _displayJournal() {
+    if(_journalMode == 0) {
+      // Editor Panel (Takes half the width)
+      return Expanded(
+        child: _buildEditor(),
+      );
+    } else {
+      // Preview Panel (Takes the other half)
+      return Expanded(
+      child: _buildPreview(),
+      );
+    }
+  }
+
+  Widget _showContent() {
+    if(_markdownContent.isEmpty){
+      return Text(
+        'Preview will appear here. Try typing # Hello World',
+        style: TextStyle(
+          color: Colors.grey,
+          fontStyle: FontStyle.italic,
+        ),
+      );
+    } else {
+      return MarkdownWidget(
+        data: _markdownContent, // fetch data from String?, which is updated by controller
+        config: MarkdownConfig(
+          configs: [ // Styles are put here
+            PConfig(
+              textStyle: const TextStyle(color: Colors.black),
+            ),
+          ],
+        ),
+      );
+    }
+  }
+
+  // ####################
+  // #### TO-DO LIST ####
+  // ####################
+  List<Task> taskList = [
+    Task(name: "Insert Task Name", date: DateTime(2025, 12, 12),  status: false),
+    Task(name: "Insert Perm Task", status: true),
+    Task(name: "Work on that long project", date: DateTime(2025, 12, 10), status: false),
+  ];
+  
+  
+  // ####################
+  // ##### CALENDAR #####
+  // ####################
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
+
+  void _onDayTapped(DateTime selectedDay, DateTime focusedDay) {
+    setState(() {
+      _selectedDay = selectedDay;
+      _focusedDay = focusedDay;
+    });
+    _handleDaySelection(selectedDay);
+  }
+
+  void _handleDaySelection(DateTime day) {
+
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text('Selected day: ${day.day}/${day.month}/${day.year}'),
+        duration: const Duration(milliseconds: 1500),
+      )
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+            title: Center(
+                child: Text(_currentDate ?? DateFormat('MMMM d, yyyy').format(DateTime.now()))
+            ),
+            bottom: const TabBar(
+              tabs: [
+                Tab(text: 'Journal'),
+                Tab(text: 'To-Do List'),
+                Tab(text: 'Calendar')
+              ],
+              labelStyle: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold
+              ),
+              unselectedLabelStyle: TextStyle(
+                color: Colors.black54,
+                fontWeight: FontWeight.normal,
+              ),
+            ),
+          ),
+          body: TabBarView(
+            children: [
+              Scaffold( // JOURNAL  ################################################################
+                body: _displayJournal(),
+                floatingActionButton: FloatingActionButton(
+                  onPressed: _toggleJournal,
+                  tooltip: 'Toggle Journal Mode',
+                  child: Icon(Icons.remove_red_eye),
+                )
+              ),
+
+              Scaffold( // TO-DO LIST ################################################################
+                body: ListView.separated(
+                  padding: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+                  itemCount: taskList.length,
+                  itemBuilder: (context, index) {
+                    var currentTask = taskList[index];
+                    return Task(key: ValueKey(currentTask.name),
+                      name: currentTask.name,
+                      date: currentTask.date,
+                      status: currentTask.status,
+                    );
+                  },
+                  separatorBuilder: (context,index) => const SizedBox(height:10),
+                ),
+                floatingActionButton: FloatingActionButton(
+                  onPressed: () async {
+                    final newTask = await showDialog<Task>(
+                      context: context,
+                      builder: (context) => const AddTaskDialog(),
+                    );
+
+                    if (newTask != null) {
+                      setState(() {
+                        taskList.add(newTask);
+                      });
+                    }
+                  },
+                  tooltip: 'Add New Task',
+                  child: Icon(Icons.add),
+                )
+              ),
+              Scaffold( // CALENDAR #########################################################################
+                body: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TableCalendar(
+                    // --- Configuration ---
+                    focusedDay: _focusedDay,
+                    firstDay: DateTime.utc(2020, 1, 1), // Start of the calendar range
+                    lastDay: DateTime.now(), // End of the calendar range
+                    calendarFormat: CalendarFormat.month, // Show month view
+                    headerStyle: const HeaderStyle(
+                      formatButtonVisible: false, // Hide the format switcher
+                      titleCentered: true,
+                    ),
+
+                    // --- Selection Logic ---
+                    selectedDayPredicate: (day) {
+                      // Use `isSameDay` to check if the current day in the builder
+                      // is the same as the stored selected day.
+                      return isSameDay(_selectedDay, day);
+                    },
+
+                    // **This is the main callback you need**
+                    onDaySelected: _onDayTapped,
+
+                    // Optional: Custom styling for the selected day
+                    calendarStyle: CalendarStyle(
+                      selectedDecoration: BoxDecoration(
+                        color: Theme.of(context).primaryColor,
+                        shape: BoxShape.circle,
+                      ),
+                      todayDecoration: BoxDecoration(
+                        color: Theme.of(context).primaryColor.withValues(alpha:0.5),
+                        shape: BoxShape.circle,
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildEditor() {
+    return Container(
+      padding: const EdgeInsets.all(16.0),
+      child: TextField(
+        controller: _markdownController,
+        decoration: const InputDecoration(
+          hintText: 'Start writing your note here...',
+          border: InputBorder.none, // Removes the default border
+        ),
+        maxLines: null, // Allows for unlimited lines
+        expands: true, // Takes up all available space in its parent
+        keyboardType: TextInputType.multiline,
+        style: const TextStyle(fontFamily: 'monospace'),
+      ),
+    );
+  }
+
+  Widget _buildPreview() {
+    return Container(
+      color: Colors.grey[50], // Slightly different background for preview
+      padding: const EdgeInsets.all(16.0),
+      child: _showContent(),
+    );
+  }
+
+}
+
+// ADD TASK
+
---
+++ lib/task.dart
@@ -0,0 +1,199 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class Task extends StatefulWidget {
+  final String name;
+  final DateTime? date;
+  final bool status;
+
+  const Task({super.key, required this.name, this.date, required this.status});
+
+  factory Task.fromJson(Map<String, dynamic> json) {
+    final String name = json['name'] as String;
+    final bool status = json['status'] as bool;
+    final DateTime? date = json['date'] != null
+      ? DateTime.parse(json['date'] as String)
+      : null;
+    return Task(
+      name: name,
+      date: date,
+      status: status,
+    );
+  }
+
+  @override
+  State<Task> createState() => _TaskState();
+
+}
+
+class _TaskState extends State<Task> {
+  late String name;
+  late DateTime? date;
+  late bool status;
+
+  @override
+  void initState() {
+    super.initState();
+    name = widget.name;
+    date = widget.date;
+    status = widget.status;
+  }
+
+  String _displayDate() {
+    if(date != null){
+      return DateFormat('Until MMMM d, yyyy').format(date ?? DateTime.now());
+      // technically it shouldn't ever get here while being null
+    }
+    return 'Permanent';
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return Container(
+      width: 100,
+      height: 80,
+      decoration: BoxDecoration(
+        color: Color.fromRGBO(215,201,245, 1),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Expanded(
+            flex: 2,
+            child: Padding(
+              padding: EdgeInsetsDirectional.fromSTEB(15, 10, 0, 0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    _displayDate(),
+                   style: TextStyle(fontWeight: FontWeight.normal),
+                  ),
+                  Text(
+                    name,
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                    overflow: TextOverflow.fade,
+                    softWrap: false,
+                  )
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Padding(
+              padding: EdgeInsets.only(left: 20),
+              child: Transform.scale(
+                  scale: 1.25,
+                  child: Checkbox(
+                    value: status,
+                    onChanged: (bool? newValue) {
+                      setState(() {
+                        status = newValue ?? false;
+                        // **Important:** If you need to save the change back to the database/JSON,
+                        // you would call a callback function here (e.g., widget.onStatusChanged(_isChecked))
+                      });
+                    },
+                )
+              )
+            )
+          )
+        ],
+      ),
+
+    );
+  }
+}
+
+class AddTaskDialog extends StatefulWidget{
+  const AddTaskDialog({super.key});
+
+  @override
+  State<AddTaskDialog> createState() => _AddTaskDialogState();
+}
+
+class _AddTaskDialogState extends State<AddTaskDialog> {
+  final TextEditingController _taskController = TextEditingController();
+  DateTime? _selectedDate;
+
+  Future<void> _pickDate() async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime.now(),
+      lastDate: DateTime(2100),
+    );
+
+    if(picked != null) {
+      setState(() {
+        _selectedDate = picked;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: const Text('New Task'),
+      content: SingleChildScrollView(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextField(
+              controller: _taskController,
+              autofocus: true,
+              decoration: const InputDecoration(
+                labelText: 'Task Name',
+                hintText: 'Do homework',
+                icon: Icon(Icons.edit),
+              )
+            ),
+            const SizedBox(height:16),
+
+            ListTile(
+              contentPadding: EdgeInsets.zero,
+              leading: const Icon(Icons.calendar_today),
+              title: Text(
+                _selectedDate == null
+                  ? 'Select Due Date'
+                  : DateFormat.yMMMd().format(_selectedDate!),
+              ),
+              onTap: _pickDate,
+              trailing: _selectedDate == null
+                ? null
+                : IconButton(
+                  icon: const Icon(Icons.clear, size:20),
+                onPressed: () {
+                  setState(() {
+                    _selectedDate = null;
+                  });
+
+                }
+              )
+            ),
+          ]
+        )
+      ),
+      actions: [
+        TextButton(
+          onPressed: () => Navigator.of(context).pop(), // close dialog
+          child: const Text ('Cancel'),
+        ),
+        ElevatedButton(
+          child: const Text('Add'),
+          onPressed: () {
+            // SAVE LOGIC HERE
+            final newTask = Task(
+              name: _taskController.text,
+              date: _selectedDate,
+              status: false
+            );
+            Navigator.of(context).pop(newTask);
+          }
+        ),
+      ]
+    );
+  }
+}
